2e1144c8d493bc62bdcbc83100b868aaa073eb83
Artur Doruch <arturdoruch@interia.pl>
2016-01-19 20:24:26 +0100

    Extracted DOMUtils from HtmlUtils class.

---
+++ DOMUtils.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Artur Doruch <arturdoruch@interia.pl>
+ */
+
+namespace ArturDoruch\Util;
+
+class DOMUtils
+{
+    /**
+     * @param \DOMNode $element
+     *
+     * @return string
+     */
+    public static function getInnerHTML(\DOMNode $element)
+    { 
+        $innerHTML = ''; 
+        $children = $element->childNodes;
+    
+        foreach ($children as $child) { 
+            $innerHTML .= $element->ownerDocument->saveHTML($child);
+        }
+    
+        return $innerHTML; 
+    }
+
+    /**
+     * @param \DOMDocument $dom
+     * @param string       $className
+     *
+     * @return \DOMNodeList
+     */
+    public static function getElementsByClassName(\DOMDocument $dom, $className)
+    {
+        $xpath = new \DOMXPath($dom);
+        
+        return $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' ".$className." ')]");            
+    }       
+    
+}
