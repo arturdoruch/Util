1dc4266495ff2cec866a44efd8e5e4682dd968d9
Artur Doruch <arturdoruch@interia.pl>
2016-01-19 20:27:36 +0100

    Added methods for cleaning html code.

--- HtmlUtils.php
+++ HtmlUtils.php
@@ -5,74 +5,114 @@
 
 namespace ArturDoruch\Util;
 
-
 class HtmlUtils
 {
     /**
+     * Removes unwanted elements and tags from html code.
+     *
+     * @param string $html The html code.
+     * @param array|null $elements
+     * The array with names of html elements to remove.
+     * If array is empty, non of elements will be removed.
+     * If null, the default elements will be removed: comment, script, style.
+     *
+     * Available elements names and their meaning:
+     *  - head        head tag with html tag. Only body tag will be left
+     *  - comment     comments except IE hacks
+     *  - script      script, noscript and iframe tags
+     *  - style       style elements
+     *  - img         img tags and input tags with type of image
+     *  - input_meta  input and meta tags, except input with type of image
+     *  - all         all elements mentioned above
+     * @param bool $removeEmptyLines If true removes empty lines.
+     *
+     * @return string
+     */
+    public static function removeNoise(&$html, array $elements = null, $removeEmptyLines = true)
+    {
+        $elements = array_flip($elements ?: array('comment', 'script', 'style'));
+
+        $patterns = array();
+        $removeAll = isset($elements['all']);
+        $removeScript = isset($elements['script']) || $removeAll;
+
+        if (isset($elements['head']) || $removeAll) {
+            $patterns[] = '<\s*head[^>]*>.+<\s*/\s*head\s*>';
+            //if (preg_match('/(<\s*body[^>]*>.*<\/body>)/si', $html, $matches)) {
+            //    $html = $matches[1];
+            //}
+        }
+
+        if ($removeScript) {
+            $patterns[] = '<\s*(script|noscript|iframe)[^>]*>.*?<\s*/\s*\1\s*>';
+            //$patterns[] = '<\s*(script|noscript|iframe)\b[^<]*(?:(?!<\/\1>)<[^<]*)*<\s*\/\s*\1\s*>';
+        }
+
+        if (isset($elements['comment']) || $removeAll) {
+            self::removeComments($html, $removeScript);
+        }
+
+        if (isset($elements['style']) || $removeAll) {
+            $patterns[] = '<\s*style[^>]*>.*?<\s*/\s*style\s*>';
+        }
+
+        if (isset($elements['input_meta']) || $removeAll) {
+            $patterns[] = '<\s*(meta|input)(?![^>]*type="image")[^>]+>';
+        }
+
+        if (isset($elements['img']) || $removeAll) {
+            $patterns[] = '<\s*img[^>]*>';
+            $patterns[] = '<\s*input[^>]+type="image"[^>]+>';
+        }
+
+        foreach ($patterns as $pattern) {
+            $html = preg_replace('@' . $pattern . '@is', '', $html);
+        }
+
+        if ($removeEmptyLines === true) {
+            StringUtils::removeEmptyLines($html);
+        }
+    }
+
+    /**
+     * Minify the html code.
+     *
      * @param string $html
-     * @param bool   $removeImage
+     * @param bool   $removeComments Remove html comments except Internet Explorer hacks.
      *
      * @return string
      */
-    public static function removeNoise($html, $removeImage = true)
+    public static function minify(&$html, $removeComments = true)
     {
-        $noise = array(
-            '<!(?=.*--.*)[^>]{4,}>',
-            '<!\[CDATA\[[^\]]+\]\]>',
-            '<\s*(script|noscript|iframe)[^>]*>[^>]*<\s*\/\s*\1\s*>',
-            '<\s*(meta|input)(?!.*type="image")[^>]+>'
-        );
+        $html = preg_replace(array(
+                '/>\s+/',
+                '/\s+</',
+                '/[\n\t\r]/',
+                '/\s{2,}/'
+            ), array(
+                '>',
+                '<',
+                '',
+                ' '
+            ), $html);
 
-        if ($removeImage === true) {
-            $noise[] = '<\s*img[^>]+>';
-            $noise[] = '<\s*input[^>]+type="image"[^>]+>';
+        if ($removeComments === true) {
+            self::removeComments($html);
         }
-
-        foreach ($noise as $pattern) {
-            $html = preg_replace("@{$pattern}@is", '', $html);
-        }
-
-        return $html;
+        //$html = str_replace("><", ">\n<", $html);
     }
 
     /**
-     * @param string $html
+     * Remove comments from html code.
      *
-     * @return string
+     * @param string $html The html code
+     * @param bool   $removeIEHacks
      */
-    public static function removeWhiteSpace($html)
+    public static function removeComments(&$html, $removeIEHacks = false)
     {
-        return preg_replace('/(?<=>)\s+(?=<)|(?<=>)\s+(?!=<)|(?!<=>)\s+(?=<)/i', '', $html);
+        $regexpPart = $removeIEHacks === false ? '(?!\[if.*endif\])' : '';
+
+        $html = preg_replace('/<!--'. $regexpPart .'.*?-->/is', '', $html);
     }
 
-    /**
-     * @param \DOMNode $element
-     *
-     * @return string
-     */
-    public static function getInnerHTML(\DOMNode $element)
-    { 
-        $innerHTML = ''; 
-        $children = $element->childNodes;
-    
-        foreach ($children as $child) { 
-            $innerHTML .= $element->ownerDocument->saveHTML($child);
-        }
-    
-        return $innerHTML; 
-    }
-
-    /**
-     * @param \DOMDocument $dom
-     * @param string       $className
-     *
-     * @return \DOMNodeList
-     */
-    public static function getElementsByClassName(\DOMDocument $dom, $className)
-    {
-        $xpath = new \DOMXPath($dom);
-        
-        return $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' ".$className." ')]");            
-    }       
-    
 }
