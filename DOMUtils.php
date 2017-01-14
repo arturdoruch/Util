219cbb95ff66b13b9b61d5c984f2fe2087de992a
Artur Doruch <arturdoruch@interia.pl>
2017-01-14 04:19:48 +0100

    DOMUtils added new methods.

--- DOMUtils.php
+++ DOMUtils.php
@@ -8,6 +8,57 @@ namespace ArturDoruch\Util;
 class DOMUtils
 {
     /**
+     * Creates a new DOMXPath from HTML content.
+     *
+     * @param string $content HTML content.
+     *
+     * @return \DOMXPath
+     */
+    public static function createXPath($content)
+    {
+        $content = mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8');
+
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        @$dom->loadHTML($content);
+
+        return new \DOMXPath($dom);
+    }
+
+    /**
+     * @param \DOMXPath|string $source
+     * @param string           $query The xpath query.
+     *
+     * @return \DOMNodeList|\DOMElement[]|null
+     */
+    public static function getNodeList($source, $query)
+    {
+        if (is_string($source)) {
+            $source = static::createXPath($source);
+        } elseif (!$source instanceof \DOMXPath) {
+            throw new \InvalidArgumentException(sprintf(
+                    'Parameter $source must be type of string or instance of DOMXPath. But given "%s"', gettype($source)
+                ));
+        }
+
+        $path = $source->query($query);
+
+        return $path && $path->length > 0 ? $path : array();
+    }
+
+    /**
+     * @param \DOMXPath|string $xpath
+     * @param string           $query The xpath query.
+     *
+     * @return \DOMElement|null
+     */
+    public static function getNode($xpath, $query)
+    {
+        $path = static::getNodeList($xpath, $query);
+
+        return !empty($path) ? $path->item(0) : null;
+    }
+
+    /**
      * @param \DOMNode $element
      *
      * @return string
